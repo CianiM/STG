b114d5f5184e735b3156b72ad46fb5bd6f867af4
CianiM <michelangelo.ciani@outlook.com>
2022-04-06 10:36:02 +0200

    update

--- genRhs.py
+++ genRhs.py
@@ -2,7 +2,9 @@ import sys
 import re
 import numpy as np
 from genKer import  genrk3, genrk3update, genFilter, genBC, append_Rhs
-import os 
+import os
+
+from generate.equations import Src_BC_dif 
 
 wp = 'float64'
   
@@ -36,19 +38,20 @@ def main():
     #append_Rhs(divF, 3,2, rhsname, vnamesrc_divFx, update=False,rhs=rhs,stored=True)                           
 
 # Generate Filters (if required):      
-    genFilter(5,4, len(varsolved),rhs=rhs)
+#    genFilter(5,4, len(varsolved),rhs=rhs)     #No filter required
 
 # Generate BCs:
     genBC(Src_conv ,5,4,rhsname , locname_conv, update=False,rhs=rhs,stored=False)
     genBC(Src_dif  ,3 ,2 ,rhsname , locname_dif , update=True ,rhs=rhs,stored=True)
     
-    # j1
-    genBC(src_BC['j1']  ,3,2,rhsname , locname_bc, setbc=[True,{'Wall_BC':{'j1':['q']}}]  , update=False,rhs=rhs)
+    #--j1
+    genBC(Src_BC['j1']  ,3,2,rhsname , locname_bc, setbc=[True,{'Wall_BC':{'j1':['q']}}]  , update=False,rhs=rhs)
+    genBC(Src_BC_conv['j'] , 3,2,rhsname, locname_bc_conv, setbc=[True, {'Symm_Wall':{'j1':['rhs']}}],  update=False, rhs=rhs )
+    genBC(Src_BC_dif['j1'], 3,2, rhsname, locname_bc_dif, setbc=[True, {'Symm_Wall':{'j1':['rhs']}}]], update=False, rhs=rhs )
 
 
-    # genBC(Src_phybc_q1  ,5,4,rhsname , vnamesrc_bc, setbc=[True,{'wall':{'j1':['q'  ]}}]  , update=False,rhs=rhs)
-
-    # # jmax
+    #--jmax
+    genBC(Src_BC_)
     # genBC(Src_phybc_rhs ,5,4,rhsname , vnamesrc_bc, setbc=[True,{'wall':{'jmax':['rhs']}}], update=False,rhs=rhs)
     # genBC(Src_phybc_qmax,5,4,rhsname , vnamesrc_bc, setbc=[True,{'wall':{'jmax':['q'  ]}}], update=False,rhs=rhs)
     
@@ -65,7 +68,7 @@ def main():
     # genBC(Src_phybc_rhs ,5,4,rhsname , locname_bc, setbc=[True,{'wall':{'i1':['rhs']}}]  , update=False,rhs=rhs)
     # genBC(Src_phybc_q1  ,5,4,rhsname , locname_bc, setbc=[True,{'wall':{'i1':['q'  ]}}]  , update=False,rhs=rhs)
     # imax
-    # genBC(Src_phybc_rhs ,5,4,rhsname , locname_bc, setbc=[True,{'wall':{'imax':['rhs']}}], update=False,rhs=rhs)      
+    # genBC(Src_phybc_rhs ,5,,4,rhsname , locname_bc, setbc=[True,{'wall':{'imax':['rhs']}}], update=False,rhs=rhs)      
     # genBC(Src_phybc_qmax,5,4,rhsname , locname_bc, setbc=[True,{'wall':{'imax':['q'  ]}}], update=False,rhs=rhs)      
 
 
